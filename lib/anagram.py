a0fb0e8d28974d51b53b73fc7c0e80c25707fbb6
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-09 16:28:45 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,17 +1,10 @@
-# your code goes here!
 class Anagram:
     def __init__(self, word):
-        self.word = word.lower()
+        self.word = word
 
     def match(self, list):
-        anagram = []
-        for list_word in list:
-            if sorted(self.word) == sorted(list_word.lower()):
-                anagram.append(list_word)
-        return anagram
-    
-
-    #The sorted() function is used to create sorted lists of characters
-    #for both self.word and list_word.
-    #If the sorted lists are equal, it means the words contain the same 
-    #letters, even if those letters are arranged differently, indicating an anagram.
+        anagrams = []
+        for i in list:
+            if sorted(self.word) == sorted(i):
+                anagrams.append(i)
+        return anagrams
